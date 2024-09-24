f285bfbb39d74a24e2c99d7240746f2d7e2947dc
Arjun Parajuli <104476004+ArjunParajuli@users.noreply.github.com>
2024-09-25 00:40:03 +0530

    login signup api call
    Gathered all input data and simulated an api call.

--- src/index.js
+++ src/index.js
@@ -5,13 +5,15 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { EventContextProvider } from './context/eventContext';
 import { BlogContextProvider } from "./context/blogContext";
-
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
   <EventContextProvider>
     <BlogContextProvider>
+      <ToastContainer />
       <App />
     </BlogContextProvider>
   </EventContextProvider>
