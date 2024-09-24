4f38524aebe90ddf4f7bcae455317148378b9800
phoneix12356 <95468972+phoneix12356@users.noreply.github.com>
2024-09-24 20:54:05 +0530

    adding comment to app.js

--- src/App.js
+++ src/App.js
@@ -25,6 +25,7 @@ function App() {
 
   return (
     <div className='bg-secondary-light font-lato flex flex-col min-h-screen'>
+      {/* <h1>Hello</h1> */}
       {!isLoginOrSignup && <header className='sticky top-0 z-20'>
         <Header />
       </header>}
