06fcedf0cd4e93f0844e4d79bc09a5c123bb7461
Abhishek Mishra <abhishekmishra0409@gmail.com>
2024-10-18 22:43:01 +0530

    Update axios.js

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://maa-foundation-backend-2-81ph.onrender.com/api";
+const BASE_URL = "https://maa-foundation-backend-wref.onrender.com/api";
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
